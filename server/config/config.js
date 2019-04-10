1bad4acf0c72022efba6984c0432458cea0e0310
Alex Cohn <alexcohnb19@ufl.edu>
2019-04-10 11:42:36 -0400

    Testing Purpose

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@
 //mongodb://adnan:episkeys1@ds219055.mlab.com:19055/boot3
 module.exports = {
     db: {
-      uri: 'mongodb://Josh:NUDNik18!@ds015335.mlab.com:15335/joshswerdlow', //place the URI of your mongo database here.
+      uri: 'mongodb://testUser:password123@ds241493.mlab.com:41493/group2_swe', //place the URI of your mongo database here.
     },
     port: process.env.PORT || 8080
   };
